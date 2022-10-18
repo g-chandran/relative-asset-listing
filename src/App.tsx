05af3368052485142de36743031f45b7feb2b30e
Hemachandran G <chandranchan321@gmail.com>
2022-10-18 23:58:57 +0530

    Added activity icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { Assets } from "./lib/components/Assets";
+import activityIcon from "./lib/data/icons/activity.png";
 
 function App() {
   return (
-    <main className="flex flex-col justify-center items-center mx-16 my-8">
-      <p className="font-tomorrow text-slate-100 w-full">Trending assets</p>
+    <main className="flex flex-col justify-center items-center mx-16 mt-40 mb-8">
+      <div className="w-full flex gap-3 items-center">
+        <img src={activityIcon} alt="activity" width="16px" height="16px" />
+        <p className="font-tomorrow text-slate-100">Trending assets</p>
+      </div>
       <Assets />
     </main>
   );
