3eb521d7e684c99f66aeb9cfbcb653fcca4834f8
Hemachandran G <chandranchan321@gmail.com>
2022-10-18 20:27:38 +0530

    Extended theme with custom fonts & colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 function App() {
-  return <div className="text-green-500">Hello world</div>;
+  return (
+    <div className="text-green-500 font-tomorrow text-5xl">Hello world</div>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bright-green": "#00FFA3",
+        "bright-red": "#FF4D4D",
+        "custom-blue": {
+          100: "#737BAE",
+          500: "#5A5F7D",
+          900: "#14172B",
+        },
+      },
+      fontFamily: {
+        tomorrow: ["Tomorrow", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
